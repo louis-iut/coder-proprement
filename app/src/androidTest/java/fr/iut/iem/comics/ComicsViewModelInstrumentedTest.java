3ca0c189b3aa18338493d5d40478bf98a3f8fe10
Quentin G <quentin01960@orange.fr>
2017-12-13 17:59:53 +0100

    [TASK] Refactor

--- app/src/androidTest/java/fr/iut/iem/comics/ComicsViewModelInstrumentedTest.java
+++ app/src/androidTest/java/fr/iut/iem/comics/ComicsViewModelInstrumentedTest.java
@@ -10,33 +10,12 @@ import fr.iut.iem.comics.data.model.remote.Comics;
 import fr.iut.iem.comics.presentation.viewmodel.ComicsViewModel;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Created by QuentinGenevois on 13/12/2017.
  */
-/*"id": 64966,
-            "title": "Despicable Deadpool (2017) #288",
-            "issueNumber": 288,
-            "description": "DEADPOOL KILLS CABLE Parts 1 and 2. Deadpool’s going back to his vile beginnings as a mercenary and wanted killer. No more being a hero. No more X-Men. No more Avengers. And if he wants to break bad again, what’s more despicable than killing your best friend? Cable better get ready because his old friend Wade has him in the crosshairs. PLUS: Includes 3 bonus MARVEL PRIMER PAGES! Story by Robbie Thompson and a TBA artist!",
-            "diamondCode": "AUG170836",
-            "date": "2017-10-25T00:00:00-0400",
-            "price": 3.99,
-            "pageCount": 32,
-            "image": "http://i.annihil.us/u/prod/marvel/i/mg/f/03/59e7b08528560.jpg",
-            "creators": [
-                {
-                    "name": "Gerry Duggan",
-                    "role": "writer"
-                },
-                {
-                    "name": "Nicholas Filardi",
-                    "role": "colorist"
-                },
-                {
-                    "name": "Scott Koblish",
-                    "role": "inker"
-                }
-            ] */
+
 public class ComicsViewModelInstrumentedTest {
 
     private Context context;
@@ -53,12 +32,16 @@ public class ComicsViewModelInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("fr.iut.iem.comics", appContext.getPackageName());
     }
 
     @Test
     public void getData_isCorrect() {
         assertEquals("Wednesday 25 October 2017", comicsViewModel.getDate(context));
+    }
+
+    @Test
+    public void getData_isIncorrect() {
+        assertNotEquals("mercredi 25 Octobre", comicsViewModel.getDate(context));
     }
 }
